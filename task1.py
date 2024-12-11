dee42b5ce4991dcaea59f87727d02e350e01a700
sarmyk <litvova2@gmail.com>
2024-12-11 11:24:35 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+# Задача 1
+print('1st program')
+print(9 ** 0.5 * 5)
+
+# Задача 2
+print('2nd program')
+print(9.99 > 9.98 and 1000!=1000.1)
+
+# Задача 3
+print('3rd program')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# Задача 4
+print('4th program')
+print(int(float('123.456') * 10) % 10)
