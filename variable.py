dc7ba14f2b4f0e1060d601e42ed81b9e43b2fad5
Владимир Литовченко <litvova2@gmail.com>
2024-12-11 12:50:53 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+count_of_tasks = 12
+count_of_hours = 1.5
+name_of_course = 'Python'
+time_of_task = count_of_hours / count_of_tasks
+print('Курс: ', name_of_course, ' всего задач: ', count_of_tasks, ', затрачено часов: ', count_of_hours, ', среднее время выполнения ', time_of_task, ' часа')
