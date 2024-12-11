7cce5206b969112e3db4ed8d8c771b4656501a2f
Владимир Литовченко <litvova2@gmail.com>
2024-12-11 12:37:04 +0300

    module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,7 @@ name = "Vladimir"
 print ('Name:', name)
 age = 35
 print ('Age: ', age)
-a = age
-age = a + 3
+age = age + 3
 print ('New age: ', age)
 is_student = True
 print ('Is Student: ', is_student)
