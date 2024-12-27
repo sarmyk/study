4f6c077ee853f3421a9850c40f0aa9355fbbf351
Владимир Литовченко <litvova2@gmail.com>
2024-12-27 15:37:45 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,14 @@
+def get_matrix(n,m , value):
+    matrix = []
+    for i in range(1, n+1):
+        matrix.append([])
+        for j in range(1, m+1):
+            matrix[i-1].append(value)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
